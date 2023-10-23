74fd7cd74bfb24664f973cf52e63d4e85c8addfa
Greypolanco <greilyntejada03@gmail.com>
2023-10-22 20:23:41 -0400

    first commit

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.DAL{
+
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+
+}
